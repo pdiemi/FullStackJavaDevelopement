321221c196bd35db9547bd0c7dbf43da3860d4f0
pdiemi <phamdiem0902@gmail.com>
2021-01-04 12:46:21 -0600

    updated login function to use userList isntead of hardcording

--- springbootDemo/src/main/java/com/demoapp/springbootDemo/UserService.java
+++ springbootDemo/src/main/java/com/demoapp/springbootDemo/UserService.java
@@ -10,7 +10,8 @@ public class UserService {
 	private HashMap<String, String> userList = new HashMap();
 
 	public boolean loginValid(String username, String password) {
-		if (username.equals("admin") && password.equals("manager")) {
+		//if (username.equals("admin") && password.equals("manager")) {
+		if (userList.get(username).equals(password)) {
 			return true;
 		}
 		return false;
