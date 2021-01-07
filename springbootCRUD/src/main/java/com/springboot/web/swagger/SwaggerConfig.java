300334f739337c9e227293b975bf34072f18d199
pdiemi <phamdiem0902@gmail.com>
2021-01-07 12:16:18 -0600

    added swagger to springbootCRUD(springboot-firstwebapp)

---
+++ springbootCRUD/src/main/java/com/springboot/web/swagger/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.springboot.web.swagger;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	public Docket postsAPI() {
+		// return new
+		// Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(apiInfo()).select().paths(postPaths()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.springboot.web")).build();
+	}
+
+	private Predicate<String> postPaths() {
+		return or(regex("/api/posts.*"), regex("/list-todos.*"));
+	}
+}
