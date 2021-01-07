f724a55d0893fae7671f624934d3e835e9aae5f1
pdiemi <phamdiem0902@gmail.com>
2021-01-07 11:49:43 -0600

    updated swagger example

--- springbootDemoSwagger/src/main/java/com/demoapp/springbootDemo/SwaggerConfig.java
+++ springbootDemoSwagger/src/main/java/com/demoapp/springbootDemo/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicate;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +17,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 	public Docket postsAPI() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(ApiInfo()).select()
-				.paths(postPaths()).build();
+		// return new
+		// Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(apiInfo()).select().paths(postPaths()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.dempapp.springbootDemo")).build();
 	}
 
 	private Predicate<String> postPaths() {
